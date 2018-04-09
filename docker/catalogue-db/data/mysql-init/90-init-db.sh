be87f5e1fce7422549562643b76e664e18240c1c
Florin Peter <github@florin-peter.de>
2018-04-09 21:40:12 +0200

    Update 90-init-db.sh

--- docker/catalogue-db/data/mysql-init/90-init-db.sh
+++ docker/catalogue-db/data/mysql-init/90-init-db.sh
@@ -2,7 +2,7 @@ init_arbitrary_database() {
   local thisdir
   local init_data_file
   thisdir=$(dirname ${BASH_SOURCE[0]})
-  init_data_file=$(readlink -f ${thisdir}/../data.sql)
+  init_data_file=$(readlink -f ${thisdir}/../dump.sql)
   log_info "Initializing the arbitrary database from file ${init_data_file}..."
   mysql $mysql_flags ${MYSQL_DATABASE} < ${init_data_file}
 }
